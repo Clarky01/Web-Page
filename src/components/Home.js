957cca1ad021769c0e00200859d7805a3751dadc
Clarky01 <clrkjester20@gmail.com>
2024-10-16 22:35:15 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div className="container">
       <h1>Welcome to My Profile!</h1>
-      <p>Hi. I am Clark Jester C. Almanon, 20 years old and currently studying at University of Cabuyao.</p>
+      <p>Hi. I am Clark Jester C. Almanon, 20 years old and currently studying at University of Cabuyao.
+      As a student at the University of Cabuyao, I’m focused on building a career that aligns with my passions and academic pursuits. I aim to excel in my chosen field, gaining practical experience and developing the skills that will contribute to my long-term success. With a clear vision of my goals, I’m driven to make a positive impact in my industry, continuously learning and adapting to new challenges along the way.
+      </p>
     </div>
   );
 }
