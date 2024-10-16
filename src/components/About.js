f893d7e30f0b34e7cd1d6f86808890c8109fbccd
Clarky01 <clrkjester20@gmail.com>
2024-10-16 22:39:21 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="container">
       <h1>About Me</h1>
-      <p>Hi, I'm Clark Jester Almanon. I enjoy coding and have a background in Java.</p>
+      <p>Hi, I'm Clark Jester Almanon. I enjoy coding and have a background in Java. I'm also a student of University of Cabuyao and I don't have any work experience as of now.</p>
     </div>
   );
 }
