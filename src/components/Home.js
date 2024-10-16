004adda8134cbf8482d5fd0089ad7df7bcbdd8ee
Clarky01 <clrkjester20@gmail.com>
2024-10-16 18:42:28 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="container">
       <h1>Welcome to My Profile!</h1>
-      <p>This is a brief introduction to my career goals.</p>
+      <p>Hi. I am Clark Jester C. Almanon, 20 years old and currently studying at University of Cabuyao.</p>
     </div>
   );
 }
