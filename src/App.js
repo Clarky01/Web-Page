0f306eac5b294a82f39093d3d597474db7e71392
Clarky01 <clrkjester20@gmail.com>
2024-10-17 10:55:49 +0800

    update css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import './App.css';
+
 
 function App() {
   return (
